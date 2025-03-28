79f7ec40b36b820ff49d0d9717d02391a5d02bc0
Isabella <isabellaj.lizarde@gmail.com>
2025-03-28 07:02:49 -0700

    fix: update email user

--- js/script.js
+++ js/script.js
@@ -1,18 +1,6 @@
-const emailUser = "isabellalizarde_27";
-const emailDomain = "yahoo.com";
+const emailUser = "mikedaly06";
+const emailDomain = "gmail.com";
 
 document
   .getElementById("contact__email")
   .setAttribute("href", `mailto:${emailUser}@${emailDomain}`);
-
-// function updateImageSrc() {
-//   const img = document.querySelector(".intro__photo");
-//   if (window.innerWidth < 768) {
-//     img.src = "/images/daly-portrait-mobile.jpg";
-//   } else {
-//     img.src = "/images/daly-portrait.jpg";
-//   }
-// }
-
-// window.addEventListener("load", updateImageSrc);
-// window.addEventListener("resize", updateImageSrc);
