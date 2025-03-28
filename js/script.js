8d31ea83ab02461755690ea83a0ee10faa642b01
Isabella <isabellaj.lizarde@gmail.com>
2025-03-28 06:43:18 -0700

    feat: dynamically change to responsive image
    will change on small screen sizes

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ const emailDomain = "yahoo.com";
 document
   .getElementById("contact__email")
   .setAttribute("href", `mailto:${emailUser}@${emailDomain}`);
+
+// function updateImageSrc() {
+//   const img = document.querySelector(".intro__photo");
+//   if (window.innerWidth < 768) {
+//     img.src = "/images/daly-portrait-mobile.jpg";
+//   } else {
+//     img.src = "/images/daly-portrait.jpg";
+//   }
+// }
+
+// window.addEventListener("load", updateImageSrc);
+// window.addEventListener("resize", updateImageSrc);
