d9b6a56c4a71351c6151c3dc5904e0a66bfa92d1
Isabella <isabellaj.lizarde@gmail.com>
2025-04-01 17:47:31 -0700

    refactor: refine style and restructure html

--- js/script.js
+++ js/script.js
@@ -4,3 +4,20 @@ const emailDomain = "gmail.com";
 document
   .getElementById("contact__email")
   .setAttribute("href", `mailto:${emailUser}@${emailDomain}`);
+
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("fade-in");
+      } else {
+        entry.target.classList.remove("fade-in"); // Remove class when out of view
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+sections.forEach((section) => observer.observe(section));
